285a7d56e2dccc3839d0faeb501c8da58044b3f3
Junwoo Hwang <zzulu.hwang@gmail.com>
2024-07-01 14:34:42 +0900

    Add: avoid duplicate members

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Random Lunch
 
 - You have to create `constants.py` file in the same directory as `main.py` file.
-  - In `constants.py` file, you have to define `MEMBERS` variables.
-  - `MEMBERS` variable should be a list of strings. For example:
+  - In `variables.py` file, you have to define `members` and `previous_groups` variables.
+  - `members` variable should be a list of strings. For example:
     ```python
-    MEMBERS = [
+    members = [
         "Alice",
         "Bob",
         "Charlie",
         "David",
+    ]
+    ```
+  - `previous_groups` variable should be a list of lists of strings. For example:
+    ```python
+    previous_groups = [
+        ["Alice", "Bob"],
+        ["Charlie", "David"],
     ]
     ```
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-import copy
-import random
-from constants import MEMBERS
+from math import ceil
+from random import shuffle
+from variables import members, previous_groups
 
 
-members = copy.deepcopy(MEMBERS)
+MAX_MEMBER_PER_GROUP = 4
+GROUP_COUNT = ceil(len(members) / MAX_MEMBER_PER_GROUP)
 
 
-if __name__ == '__main__':
-    random.shuffle(members)
+groups = [[] for _ in range(GROUP_COUNT)]
 
-    groups = [[], [], [], [], []]
 
-    for index, user in enumerate(members):
-        groups[index % 5].append(user)
+previous_pairs = {}
+for group in previous_groups:
+    for member in group:
+        previous_pairs[member] = set(group)
+        previous_pairs[member].remove(member)
 
+
+def can_be_grouped(group: list, member: str):
+    for group_member in group:
+        if group_member in previous_pairs.get(member, set()):
+            return False
+    return True
+
+
+shuffle(members)
+for user in members:
+    shuffle(groups)
     for group in groups:
-        print(' '.join(group))
+        if can_be_grouped(group, user) and len(group) < MAX_MEMBER_PER_GROUP:
+            group.append(user)
+            break
+    else:
+        for group in groups:
+            if len(group) < MAX_MEMBER_PER_GROUP:
+                group.append(user)
+                break
+
+
+for group in groups:
+    print(' '.join(group))
